b259826a91ae1900f88f2af93e86cc523c551ad7
김승수 <supermen703@hanmail.net>
2022-06-27 19:22:57 +0900

    remove bigfile
    Former-commit-id: b94f55a9f7d59c4a464e948390ae56cd8ff428b6

---
+++ client/InSsa/index.js
@@ -0,0 +1,29 @@
+import "./css/style.css";
+import "./css/main.css";
+import "./css/loading.css";
+
+import "./assets/gltf/dove.gltf";
+import "./assets/gltf/house.glb";
+import "./assets/gltf/mallardDuck_2.gltf";
+import "./assets/gltf/mallardDuck.gltf";
+import "./assets/gltf/navmesh.glb";
+
+import "./assets/images/dove.png";
+import "./assets/images/duck.png";
+import "./assets/images/hall.jpg";
+import "./assets/images/share.png";
+import "./assets/images/loader.gif";
+
+require("./js/loading.js");
+require("./js/openModal.js");
+require("./js/color-changer.component.js");
+require("./js/forward.component.js");
+require("./js/move-in-circle.component.js");
+require("./js/persistent-p2p.component.js");
+require("./js/randomize-show-child.component.js");
+require("./js/remove-in-seconds.component.js");
+require("./js/spawn-in-circle.component.js");
+require("./js/spawner-persistent.component.js");
+require("./js/spawner.component.js");
+require("./js/toggle-ownership.component.js");
+require("./js/show-child.component.js");
